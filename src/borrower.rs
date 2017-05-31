037ce99a5e08b130f430215527653c7efa36bbde
Greg Wiley <gwiley@cj.com>
2017-05-31 16:00:51 -0700

    borrow write/read

--- src/borrower.rs
+++ src/borrower.rs
@@ -3,6 +3,10 @@ fn work_with_borrowed_vector(ints: &Vec<i32>) {
     println!("I am just borrowing these in a function: {:?}", ints);
 }
 
+fn modify_borrowed_vector(ints: &mut Vec<i32>) {
+    ints[0] += 100;
+}
+
 pub fn demo() {
 
     let v0 = vec![100, 100, 100];
@@ -16,6 +20,20 @@ pub fn demo() {
     let v1 = vec![7,8,9];
     work_with_borrowed_vector(&v1);
     println!("See? I still own them: {:?}", v1);
+
+    let mut v2 = vec![10, 11, 12];
+    println!("I can read this: {:?}", v2);
+    println!("I can read it again: {:?}", v2);
+    // let v2_borrowed_for_write = &mut v2;
+    modify_borrowed_vector(&mut v2);
+    println!("I can still read it: {:?}", v2);
+    {
+        let v2_borrowed_for_write = &mut v2;
+        v2_borrowed_for_write[1] = 9999;
+        // println!("try to read: {:?}", v2);
+        println!("I can read from the borrower: {:?}", v2_borrowed_for_write);
+    }
+    println!("Whew, now I can read it: {:?}", v2);
 
 
 }
